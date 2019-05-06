6bbaa87f7ae22b72dd7140619ff8dd75aedfe8f3
dugm <19911108>
2019-05-06 16:17:00 +0800

    单例模式demo

--- src/main/java/com/bugbean/designpattern/singleton/iodh/LoadBalancer.java
+++ src/main/java/com/bugbean/designpattern/singleton/iodh/LoadBalancer.java
@@ -1,5 +1,16 @@
 package com.bugbean.designpattern.singleton.iodh;
 
 public class LoadBalancer {
+    private static LoadBalancer loadBalancer = null;
 
+    private LoadBalancer() {
+    }
+
+    public static LoadBalancer getInstance() {
+        return LoadBalancerHolder.instance;
+    }
+
+    private static class LoadBalancerHolder {
+        private final static LoadBalancer instance = new LoadBalancer();
+    }
 }
